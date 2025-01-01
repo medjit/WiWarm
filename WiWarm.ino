bdaa350280dc52e07cd6e06011dda8c9f86948ed
Medjit Saitov <saitovmedjit@gmail.com>
2025-01-02 01:04:27 +0200

    Init hal.h

--- WiWarm.ino
+++ WiWarm.ino
@@ -1,8 +1,14 @@
+#include "hal.h"
 
 void setup() {
-
+    hal_init();
 }
 
 void loop() {
 
+    static unsigned long lastMillis = 0;
+    if (millis() - lastMillis >= 1) {
+        lastMillis = millis();
+        hal_loop();
+    }
 }
---
+++ hal.h
@@ -0,0 +1,25 @@
+#ifndef HAL_H
+#define HAL_H
+
+//TODO:
+//define pinout
+//read ds18b20
+//read dht22
+//set fan speed (pwm)
+//set buzzer
+//read buttons
+
+void hal_init();
+void hal_loop();
+
+void hal_init()
+{
+
+}
+
+void hal_loop()
+{
+
+}
+
+#endif // HAL_H
